d9fbeec7961d19392c65d6042c9fac6dfc90362b
jkraemer8624 <123784230+jkraemer8624@users.noreply.github.com>
2025-04-15 23:51:54 -0700

    Add header to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,25 @@
+/*
+Jake Kraemer, Connor Pham
+
+Video: https://youtube.com/shorts/PROw4EyoDz8
+
+Works Cited:
+        https://github.com/sparkfun/SparkFun_Qwiic_6DoF_LSM6DSO_Arduino_Library
+        https://registry.platformio.org/libraries/nkolban/ESP32%20BLE%20Arduino
+
+Part 1:
+Develop a client-server communication between ESP32 and a cellphone. 
+You can either use the onboard LED or hook one up on the breadboard. 
+Develop two commands: one to switch LED on, and another to switch LED off.
+
+Part 2:
+Using the LSM6DS3 sensor and ESP32 based TTGO, count number of steps by mimicking the step motion and sent it by Bluetooth to cell phone. Please follow the below steps:
+- You need to calibrate the sensor (Remember how you calibrate a sensor and mapped it).
+- In the loop, continuously read the data (for example each 20 ms). You can just read the data from one of the axes, or read both x and y and calculate their root mean square.
+- To count the number of steps, you need to set a threshold. Note that for each step, there might be more than one value over the threshold. Count the number of steps using this threshold. Print the number of steps in a terminal.
+- Send the number of taken steps to the cell phone using the built-in BLE.
+*/
+
 #include <Arduino.h>
 #include <Wire.h>
 #include <SparkFunLSM6DSO.h>
